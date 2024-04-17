7df8909d3320cad55138844246e8e2961e957201
Alexander Platonov <sasa20sasha19910@yandex.ru>
2024-04-17 09:37:37 +0300

    add filter_data_collection for class Aggregator

--- app/aggregator.py
+++ app/aggregator.py
@@ -1,8 +1,8 @@
-import asyncio
 from datetime import datetime
 
 import aiofiles
 import bson
+import pandas as pd
 
 
 class Aggregator:
@@ -15,10 +15,37 @@ class Aggregator:
     ):
         data_collection = await self.load_data_from_bson_async()
 
+        return self.filter_data_collection(
+            data_collection,
+            dt_from,
+            dt_upto,
+            group_type
+        )
+
     async def load_data_from_bson_async(self):
         async with aiofiles.open('../dump/sampleDB/sample_collection.bson', mode='rb') as f:
             data = await f.read()
             return bson.decode_all(data)
 
+    @staticmethod
+    def filter_data_collection(data_collection, dt_from, dt_upto, group_type):
+        if group_type in ('month', 'day', 'hour'):
+            full_range_collection = pd.date_range(start=dt_from, end=dt_upto, freq=group_type[0])
+            df_full_range = pd.DataFrame({'dt': full_range_collection})
+            df_full_range['dt'] = df_full_range['dt'].dt.to_period(group_type[0])
+
+            df_collection = pd.DataFrame(data_collection)
+            df_collection = df_collection[(df_collection['dt'] >= dt_from) & (df_collection['dt'] <= dt_upto)]
+
+            df_collection['dt'] = df_collection['dt'].dt.to_period(group_type[0])
+            df_collection = df_collection.groupby('dt')['value'].sum()
+
+            df_full_range = pd.merge(df_full_range, df_collection, on='dt', how='left')
+            df_full_range['value'] = df_full_range['value'].fillna(0).astype(int)
+
+            dataset = df_full_range['value'].tolist()
+            labels = [idx.strftime('%Y-%m-%dT%H:%M:%S') for idx in df_full_range['dt']]
+
+            return {'dataset': dataset, 'labels': labels}
 
 
